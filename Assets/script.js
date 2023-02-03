9154933fa5bae970bb43d5d8a263363208aa3b53
Reuben Schmolke <sandwichcrashers@gmail.com>
2023-02-03 09:20:26 -0800

    added initial files

---
+++ Assets/script.js
@@ -0,0 +1,19 @@
+// User is presented with 5 questions - array of objects
+    // format for questions
+        // object that contains {question: string, choices: array, answer: string}
+        // display question - loop
+        // append question
+        // append choices array
+        
+// user selects an answer(button,radio,checkboxes)data = ""
+    // click event is on the parent container
+    // how to know which element was clicked(event.target)
+    // if the answer is correct display next question access array of questions object
+    // if answer is incorrect add 15 seconds to score and display next question
+    
+    // when all questions are answered display form to submit initials
+    // save for values score and initials to localStorage
+
+// change to highscores HTML
+    // read values from localStorage
+    // append score values to page
